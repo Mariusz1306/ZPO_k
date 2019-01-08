8ca17a4031f0ce3e603cc508d5ffcf7a073fb945
Mariusz1306 <33585033+Mariusz1306@users.noreply.github.com>
2019-01-08 15:51:57 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String args[]){
-        Knight start = new Knight(2,1);
-        Knight end = new Knight(2,2);
-        Square board = new Square(8,8); //columns are from 1 to x, rows from 1 to y
+        Knight start = new Knight(2,1); //startowa pozycja konia
+        Knight end = new Knight(2,2); //końcowa pozycja konia
+        Square board = new Square(8,8); //kolumny są od 1 do x, rzędy od 1 do y
         System.out.println(board.path(start,end));
     }
 }
